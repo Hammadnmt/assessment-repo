03611ed21a3edf3d1263570cafe580d308c955d6
Hammad <hammad.alinmt@gmail.com>
2025-08-17 19:04:50 +0500

    button alignment fixed

--- components/Product.tsx
+++ components/Product.tsx
@@ -14,8 +14,8 @@ const ProductCard: React.FC<ProductCardProps> = ({ _id, name, price, description
   const { handleAddToCart } = useProductCard(name, _id, price, image);
 
   return (
-    <div className="bg-black text-white rounded-lg border border-gray-700 shadow-md max-w-sm flex flex-col">
-      <div className="h-24 w-full flex bg-gray-900 rounded-t-lg relative">
+    <div className="bg-black text-white rounded-lg border border-gray-700 shadow-md max-w-sm flex flex-col justify-between">
+      <div className="h-24 w-full flex justify-baseline bg-gray-900 rounded-t-lg relative">
         <Image src={image} alt={name} fill className="object-cover" />
       </div>
 
@@ -29,7 +29,8 @@ const ProductCard: React.FC<ProductCardProps> = ({ _id, name, price, description
           <span className="text-lg font-bold text-white">${price}</span>
           <span className="text-[10px] text-gray-400">USD/monthly</span>
         </div>
-
+      </div>
+      <div className="mb-1 p-3">
         <button
           className="bg-sky-400 text-black py-1.5 mt-2 rounded-md hover:bg-sky-300 font-semibold text-sm w-full"
           onClick={handleAddToCart}
